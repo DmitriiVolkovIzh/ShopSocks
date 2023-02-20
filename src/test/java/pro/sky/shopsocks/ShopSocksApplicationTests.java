25fc10c2a9a0a38a6ef5eaadf934d4c882a544c2
DmitriiVolkovIzh <booyakaa@mail.ru>
2023-02-20 21:36:28 +0400

    Initial commit

---
+++ src/test/java/pro/sky/shopsocks/ShopSocksApplicationTests.java
@@ -0,0 +1,13 @@
+package pro.sky.shopsocks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ShopSocksApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
